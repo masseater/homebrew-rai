05621a14e51d92bbb442197cf0c5ef7789ec56c2
masseater <ika.u1f991@gmail.com>
2026-04-28 12:18:20 +0900

    rai 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/rai.rb
+++ Formula/rai.rb
@@ -1,13 +1,13 @@
 class Rai < Formula
   desc "Extensible personal CLI"
   homepage "https://github.com/masseater/rai"
-  version "0.1.0"
+  version "0.1.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/masseater/rai/releases/download/v0.1.0/rai-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "d99477223d626b335e09d6135bb9a87406275542612d8f00a512251028d8800b"
+      url "https://github.com/masseater/rai/releases/download/v0.1.1/rai-v0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "18d4976ed9a1f185b98ce4fbf94fb12273879f5f9d9cc61477c0d5a8c7493535"
     else
       url "https://github.com/masseater/rai/releases/download/v0.1.0/rai-v0.1.0-x86_64-apple-darwin.tar.gz"
       sha256 "c01d0451cbe4af4e76a802675d1a49772cf9c27fb4dc8b27a4d116597c8c0f0d"
