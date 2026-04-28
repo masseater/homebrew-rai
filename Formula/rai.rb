a16fefdd02939775c01aad76d76350af063e8600
masseater <ika.u1f991@gmail.com>
2026-04-28 14:24:28 +0900

    rai 0.1.3 (fix: update all platforms)

--- Formula/rai.rb
+++ Formula/rai.rb
@@ -9,18 +9,18 @@ class Rai < Formula
       url "https://github.com/masseater/rai/releases/download/v0.1.3/rai-v0.1.3-aarch64-apple-darwin.tar.gz"
       sha256 "fe26c3ba0459da6ae9818541d033e4233f8e21d3e6df9c4cce16a4c03b96bcaa"
     else
-      url "https://github.com/masseater/rai/releases/download/v0.1.0/rai-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c01d0451cbe4af4e76a802675d1a49772cf9c27fb4dc8b27a4d116597c8c0f0d"
+      url "https://github.com/masseater/rai/releases/download/v0.1.3/rai-v0.1.3-x86_64-apple-darwin.tar.gz"
+      sha256 "38f0e6386ec79b23c424c0e5b874b495417d630344ae5f5b6a544c47e005e063"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/masseater/rai/releases/download/v0.1.0/rai-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "08de916b022620f8410933990b7b85232f7193f257864547b39322915a47165e"
+      url "https://github.com/masseater/rai/releases/download/v0.1.3/rai-v0.1.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "2ac3a1e391c64123209133e34f6b19e5c2e338aaec904b5713b4966e1bf39bba"
     else
-      url "https://github.com/masseater/rai/releases/download/v0.1.0/rai-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0ed383e2c421c625663f78ad10382a8479a3078a4522e440859ed9f09235f8ea"
+      url "https://github.com/masseater/rai/releases/download/v0.1.3/rai-v0.1.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ae10b26034fe00cb80e43983c7497eb77ac59d61f877e937245f2023d21cb030"
     end
   end
 
